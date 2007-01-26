babf1caadf1fa50925f403cde426f46cf17e04d9
Philipp von Weitershausen <philipp@weitershausen.de>
2007-01-26 14:26:41 +0000

    Add a question for the initial module name when creating a grok sandbox.  This is a preliminary fix for https://launchpad.net/grok/+bug/80403.

--- src/grokproject/__init__.py
+++ src/grokproject/__init__.py
@@ -10,10 +10,22 @@ class GrokProject(templates.Template):
     required_templates = []
 
     vars = [
+        var('module', 'Name of an demo Python module placed into the package',
+            default=NoDefault),
         var('user', 'Name of an initial administrator user', default=NoDefault),
         var('passwd', 'Password for the initial administrator user',
             default=NoDefault),
         ]
+
+    def check_vars(self, vars, cmd):
+        vars = super(GrokProject, self).check_vars(vars, cmd)
+        module = vars['module']
+        if '.' in module:
+            if module.endswith('.py'):
+                vars['module'] = module[:-3]
+            else:
+                raise command.BadCommand('Bad module name: %s' % module)
+        return vars
 
 def main():
     usage = "usage: %prog [options] PROJECT"
