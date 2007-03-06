09aad760eda878af54d3f016751ed44a436abefd
Philipp von Weitershausen <philipp@weitershausen.de>
2007-03-06 16:51:13 +0000

    bump revision number and designate this as a 'dev' version (there hasn't been any release of grokproject yet)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grokproject',
-    version='0.1',
+    version='0.2dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='https://launchpad.net/grok',
