502b50c472434f53c0e74b3a0fa25afffce6a0be
Philipp von Weitershausen <philipp@weitershausen.de>
2007-11-11 10:14:32 +0000

    Split changelog away from readme.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+long_description = (open('README.txt').read()
+                    + '\n\n' +
+                    open('CHANGES.txt').read())
+
 setup(
     name='grokproject',
     version='0.7dev',
@@ -8,7 +12,7 @@ setup(
     url='https://launchpad.net/grok',
     download_url='svn://svn.zope.org/repos/main/grokproject/trunk#egg=grokproject-dev',
     description='Script that sets up a grok project directory, installs Zope 3 and grok and creates a template for a grok application.',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
     license='ZPL',
 
     packages=find_packages(),
