1fe44f15572546357f50f248ea200be497b614ef
Philipp von Weitershausen <philipp@weitershausen.de>
2007-11-11 10:36:52 +0000

    No longer ask for the name of the module that will contain the   grok.Application subclass.  It's 'app.py' by default now, a rename   is easy enough to do later on.

--- grokproject/__init__.py
+++ grokproject/__init__.py
@@ -18,8 +18,6 @@ class GrokProject(templates.Template):
     required_templates = []
 
     vars = [
-        var('module', 'Name of a demo Python module placed into the package',
-            default='app.py'),
         var('user', 'Name of an initial administrator user', default=NoDefault),
         var('passwd', 'Password for the initial administrator user',
             default=NoDefault),
@@ -29,12 +27,6 @@ class GrokProject(templates.Template):
 
     def check_vars(self, vars, cmd):
         vars = super(GrokProject, self).check_vars(vars, cmd)
-        module = vars['module']
-        if '.' in module:
-            if module.endswith('.py'):
-                vars['module'] = module[:-3]
-            else:
-                raise command.BadCommand('Bad module name: %s' % module)
         if vars['package'] in ('grok', 'zope'):
             print
             print "Error: The chosen project name results in an invalid " \
