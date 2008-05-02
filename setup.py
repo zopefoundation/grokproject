3e376afb1e7654445bfd7d9cc79fc2e94925401d
Reinout van Rees <reinout@vanrees.org>
2008-05-02 15:06:21 +0000

    Depending on a newer version of paste.script as otherwise the should_echo argument raises an error. This also fixes the problem that (at least Reinout's) global paste doesn't see any grok templates.

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ setup(
     author_email='grok-dev@zope.org',
     url='https://launchpad.net/grok',
     download_url='svn://svn.zope.org/repos/main/grokproject/trunk#egg=grokproject-dev',
-    description='Script that sets up a grok project directory, installs Zope 3 and grok and creates a template for a grok application.',
+    description="""
+    Script that sets up a grok project directory, installs Zope 3 and grok and
+    creates a template for a grok application.""",
     long_description=long_description,
     license='ZPL',
 
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['PasteScript>=1.3',],
+    install_requires=['PasteScript>=1.6',],
     entry_points={
     'console_scripts': ['grokproject = grokproject:main'],
     'paste.paster_create_template': ['grok = grokproject:GrokProject']},
