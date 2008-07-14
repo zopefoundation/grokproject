5b0c62cc237e828a999d2d3f8f7ba74dea2ea1f2
Philipp von Weitershausen <philipp@weitershausen.de>
2008-07-14 03:20:49 +0000

    bump version number on the trunk after release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (open('README.txt').read()
 
 setup(
     name='grokproject',
-    version='0.8dev',
+    version='0.9dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='https://launchpad.net/grok',
