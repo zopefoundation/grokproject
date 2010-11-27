2f6cd41fc04ae385e3f725c1b01ab5b9919f513a
Vincent Fretin <vincent.fretin@gmail.com>
2010-11-27 15:37:59 +0000

    Set the correct next version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='grokproject',
-    version='ydev',
+    version='2.3dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
