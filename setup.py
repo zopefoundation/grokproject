5fde9fde5ca3dc4bdd12dfc6cc88824214da6f9e
Vincent Fretin <vincent.fretin@gmail.com>
2011-01-10 10:11:08 +0000

    Back to development: 2.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='grokproject',
-    version = '2.3',
+    version = '2.4dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
