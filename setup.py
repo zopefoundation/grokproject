aca3817a035329b6079000b75d76c81f3cc04f80
Vincent Fretin <vincent.fretin@gmail.com>
2011-01-10 10:10:16 +0000

    Preparing release 2.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='grokproject',
-    version='2.3dev',
+    version = '2.3',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
