19fec6db04da661a02654c701146ef0fd0d81127
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-05-26 11:20:18 +0000

    Preparing release 2.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='grokproject',
-    version='2.6dev',
+    version='2.6',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
